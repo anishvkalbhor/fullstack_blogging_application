5576c65141e00c3cb5d3fdb0d27195a4898720bd
Anish Kalbhor <anishkalbhor2020@gmail.com>
2025-11-03 19:47:57 +0530

    feat(layout): update page title to 'DevPress' and add feather icon SVG

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -10,7 +10,7 @@ import { Analytics } from '@vercel/analytics/react';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Full-Stack Blog',
+  title: 'DevPress',
   description: 'Tech Assessment',
 };
 
